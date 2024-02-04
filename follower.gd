d1a6415d188b2aa88bdf5e963d3ae5e52d539298
Jon Hendrix <skybound987@protonmail.com>
2024-02-04 11:26:46 -0500

    Finally figured out the timer delay on the follower.  Created a timer in the follower scene tree, set it to autostart true and 1 second wait time, connected the node to function _on_timer_timeout() which sets start_moving to true, which is initialized as false when the follower is instantiated.  If the follower is further than 25 pixels away and start_moving is true (which happens when the timer reaches zero, so the follower will wait until the timer is complete before moving) it begin moving toward the player, else if the distance is less than or equal to 25 pixels, follower velocity is set to zero and start_moving is assigned false.  Still need to correct follower angles to match the player angles.

--- follower.gd
+++ follower.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 # Follower variables
-var speed = 50.0
+var speed = 49.0
 var start_moving = false
 var player_position
 var target_position
@@ -10,6 +10,9 @@ var target_position
 var idle_animations = ["idle0", "idle1", "idle2", "idle3", "idle4", "idle5", "idle6", "idle7"]
 var walk_animations = ["walk0", "walk1", "walk2", "walk3", "walk4", "walk5", "walk6", "walk7"]
 
+func _ready():
+	$Timer.start()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var is_moving = false
@@ -17,35 +20,35 @@ func _process(delta):
 	player_position = player_node.position
 	target_position = (player_position - global_position).normalized()
 	
-	var angle = rad_to_deg(target_position.angle())
-	var animation_direction = get_direction_index(angle) 
-	var animation_name = ""  #gets animation name as string
-	
-	if global_position.distance_to(player_position) > 45 and start_moving:
+	if global_position.distance_to(player_position) > 25 and start_moving:
 		velocity = target_position * speed
 		is_moving = true
-	#	await $Timer.timeout
 		move_and_slide()
-		
+	elif global_position.distance_to(player_position) <= 25:
+		velocity = Vector2.ZERO
+		start_moving = false
 		
 #	elif global_position.distance_to(player_position) < 25:
 #		velocity = -target_position * speed
 #		move_and_slide()
-	
+
+	var angle = rad_to_deg(target_position.angle())
+	var animation_direction = get_direction_index(angle) 
+	var animation_name = ""  #gets animation name as string
+
 	if is_moving:
 		animation_name = walk_animations[animation_direction]
 	else:
 		animation_name = idle_animations[animation_direction]
 	
 	$AnimatedSprite2D.play(animation_name)
+	
 
 func get_direction_index(angle):
 	if angle < 0:
 		angle += 360
 	var direction_index = int(angle / 45) % 8
 	return direction_index
-
-
 
 func _on_timer_timeout():
 	start_moving = true
