19b458d31165eead171ce94ee7b09b73012553e5
skybound987 <skybound987@protonmail.com>
2024-02-04 00:30:32 -0500

    Corrected follower movement and timers, experimenting with different methods.  Connected _on_timer_timeout() function with node, that sets start_moving to true.

--- follower.gd
+++ follower.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 # Follower variables
 var speed = 50.0
+var start_moving = false
 var player_position
 var target_position
 @onready var player_node = get_node_or_null("/root/Main/Player")
@@ -10,8 +11,9 @@ var idle_animations = ["idle0", "idle1", "idle2", "idle3", "idle4", "idle5", "id
 var walk_animations = ["walk0", "walk1", "walk2", "walk3", "walk4", "walk5", "walk6", "walk7"]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
+func _process(delta):
 	var is_moving = false
+	
 	player_position = player_node.position
 	target_position = (player_position - global_position).normalized()
 	
@@ -19,11 +21,12 @@ func _physics_process(delta):
 	var animation_direction = get_direction_index(angle) 
 	var animation_name = ""  #gets animation name as string
 	
-	if global_position.distance_to(player_position) > 45:
-		await get_tree().create_timer(1.0).timeout
+	if global_position.distance_to(player_position) > 45 and start_moving:
 		velocity = target_position * speed
 		is_moving = true
+	#	await $Timer.timeout
 		move_and_slide()
+		
 		
 #	elif global_position.distance_to(player_position) < 25:
 #		velocity = -target_position * speed
@@ -42,3 +45,7 @@ func get_direction_index(angle):
 	var direction_index = int(angle / 45) % 8
 	return direction_index
 
+
+
+func _on_timer_timeout():
+	start_moving = true
