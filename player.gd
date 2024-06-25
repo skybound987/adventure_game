822d6086b77d47b83f27db103cf8f3e3756a19e6
Jon Hendrix <skybound987@protonmail.com>
2024-06-25 09:56:28 -0400

    Figured out the issue with attack animations, turns out that the movement animation was constantly looping so any attack animation would appear to end instantly.  Adding a branch to the movement animation statement that says if the player is not attacking, then continue animating the walking movement.

--- player.gd
+++ player.gd
@@ -1,28 +1,32 @@
 extends CharacterBody2D
 
-
 var speed = 50
 var target
 var animation_direction = 0
 var mouse_position = position
-
+var is_attacking = false
+@onready var animations = $Animation
 
 var idle_animations = ["idle0", "idle1", "idle2", "idle3", "idle4", "idle5", "idle6", "idle7"]
 var walk_animations = ["walk0", "walk1", "walk2", "walk3", "walk4", "walk5", "walk6", "walk7"]
+var attack_animations = ["attack0", "attack1", "attack2", "attack3", "attack4", "attack5", "attack6", "attack7"]
 
 func _ready():
 	pass
 
-#func _input(event):
-#	if event.is_action_pressed("mouse_1"):
-#		mouse_position = get_global_mouse_position()
 
-func _physics_process(delta):
+	
+#func _input(event):
+	#if event.is_action_pressed("mouse_1"):
+		#mouse_position = get_global_mouse_position()
+
+func _process(delta):
 	var is_moving = false
 #	var direction = (mouse_position - global_position)
 #	var angle = rad_to_deg(direction.angle())
 	var animation_name = ""  # gets animation name as string
-	
+	var is_atacking = false
+		
 	velocity = Vector2.ZERO
 
 	if Input.is_action_pressed("move_up"):
@@ -40,6 +44,15 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 		is_moving = true
+	
+	if Input.is_action_just_pressed("mouse_1") and not is_attacking:
+#		mouse_position = get_global_mouse_position()
+		is_attacking = true
+		var angle = rad_to_deg(velocity.angle())
+		animation_direction = get_direction_index(angle)
+		animation_name = attack_animations[animation_direction]
+		$Animation.play(animation_name)
+		print("attack attack attack")
 		
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")	
 	
@@ -49,6 +62,9 @@ func _physics_process(delta):
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 	
+	#if is_on_stair_tile():
+		#adjust_y_bias()
+	
 	if is_moving:
 		move_and_slide()
 		var angle = rad_to_deg(velocity.angle())
@@ -56,11 +72,15 @@ func _physics_process(delta):
 		animation_name = walk_animations[animation_direction]
 	else:
 		animation_name = idle_animations[animation_direction]
-		
-	$AnimatedSprite2D.play(animation_name)
 	
+	if not is_attacking:
+		$AnimatedSprite2D.play(animation_name)
 
 		
+func _on_animation_animation_finished(anim_name):
+	is_attacking = false
+	
+
 func start(pos):  # Initalize things for player, position, etc
 	position = pos
 	target = pos
@@ -78,7 +98,7 @@ func get_direction_index(angle):
 		return 1  # SE, making SE more sensitive
 	elif angle < 112.5:
 		return 2  # South
-	elif angle < 153.0:
+	elif angle < 165.0:
 		return 3  # SW
 	elif angle < 202.5:
 		return 4  # West
@@ -90,8 +110,17 @@ func get_direction_index(angle):
 		return 7  # NE
 
 
+#func is_on_stair_tile() -> bool:
+	## Assuming a TileMap node named "TileMap" exists in the scene
+	#var tilemap = get_node("TileMap2D")
+	#var tile_pos = tilemap.world_to_map(global_position)
+	#var tile_id = tilemap.get_cellv(tile_pos)
+	## Assuming stair tile has an ID of 1
+	#return tile_id == 1
 
-
-	
-
-	
+#func adjust_y_bias():
+	## Adjust the Y-bias based on direction and some bias factor, e.g., 20
+	#if velocity.y < 0:  # Going up
+		#velocity.y -= 20
+	#elif velocity.y > 0:  # Going down
+		#velocity.y += 20
